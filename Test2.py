99c2f31f3ed184f566633698f56c4331a1eacb7f
MarcDekkert <marc98dekker@gmail.com>
2017-02-02 15:35:51 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -38,7 +38,7 @@ GPIO.setup(led1, GPIO.OUT)
 GPIO.setup(led2, GPIO.OUT)
 #----------------------------------------
 '''Andere variabelen'''
-Grens_afstand = 25
+Grens_afstand = 27
 achteruit_tijd = 0.5
 omdraai_tijd = 0.8
 Uturn_tijd = 0.7
